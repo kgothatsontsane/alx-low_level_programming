270fa80a9f5859637ca9b536855459782962835b
Kgothatso <nvisionfactory@gmail.com>
2023-09-12 17:05:22 -0700

    revamped 0-putchar to use main.h functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,22 @@
-#include <stdio.h>
 #include "main.h"
+
+/**
+ * The main function prints _putchar,
+ * using characters followed by a new line
+ * Return value: 0 (Success)
+ */
 
 int main(void)
 {
-printf("_putchar\n");
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
