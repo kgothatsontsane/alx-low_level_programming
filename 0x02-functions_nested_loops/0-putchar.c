626612167d897ad4e60106bb17b1f2fd3c895aa0
Kgothatso <nvisionfactory@gmail.com>
2023-09-12 17:45:24 -0700

    rep broken?

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
+
 #include "main.h"
 
 /**
- * The main function prints _putchar,
- * using characters followed by a new line
- * Return value: 0 (Success)
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 
 int main(void)
