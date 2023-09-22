7a243d8e662f2a4c7e8e290911fcf2cd1e0d9687
Kgothatso <nvisionfactory@gmail.com>
2023-09-22 05:46:56 -0700

    changes to 102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
 
-/**
- *102-magic.c
- *
- * main - prints value of a[2] to be 98
- * This program initializes an array 'a' and a pointer 'p'. It then sets
- * the value of a[2] to 1024, assigns the address of the integer 'n' to
- * pointer 'p', and modifies the value at *(p + 5) to be 98. Finally,
- * it prints the value of a[2] to the standard output.
- * Return: 0
-*/
-
 int main(void)
 {
 	int n;
