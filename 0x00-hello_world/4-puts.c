dd7db3c5d9ae4bb9236305cc0002ea633cb0bfc1
Kgothatso <nvisionfactory@gmail.com>
2023-09-23 01:26:22 -0700

    added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-
 /**
- * main - prints the string in puts
+ * main - outputs the string in puts
  *
- * Return = 0
+ * Return: 0
  */
+
 int main(void)
 {
 	int puts(const char *s);
-	puts("Programming is like building a multilingual puzzle");
+
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
