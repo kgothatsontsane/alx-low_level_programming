2c0cb151e8e7e8fc080f70325edd2d745da6e727
Kgothatso <nvisionfactory@gmail.com>
2023-10-12 20:23:11 -0700

    changes to variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,8 @@
-#ifndef VARIADIC_H_
-#define VARIADIC_H_
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
