73137563967c6e77b614005e540f0597c833f9ae
Kgothatso <nvisionfactory@gmail.com>
2023-10-19 16:05:06 -0700

    removed space from Return description in task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,7 +9,7 @@
  *
  * @str: string value data member of new node
  *
- *  Return: the address of the new element, or NULL if it failed
+ * Return: the address of the new element, or NULL if it failed
  */
 
 list_t *add_node_end(list_t **head, const char *str)
