1d5a3a868ae4d22f618b3bec066ab572d177930f
Kgothatso <nvisionfactory@gmail.com>
2023-10-24 13:41:12 -0700

    edited task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -6,35 +6,36 @@
  *
  * Return: the size of the list that was freed
  */
+
 size_t free_listint_safe(listint_t **h)
 {
-        size_t length = 0;
-        int difference;
-        listint_t *temp;
+	size_t length = 0;
+	int difference;
+	listint_t *temp;
 
-        if (!h || !*h)
-                return (0);
+	if (!h || !*h)
+		return (0);
 
-        while (*h)
-        {
-                difference = *h - (*h)->next;
-                if (difference > 0)
-                {
-                        temp = (*h)->next;
-                        free(*h);
-                        *h = temp;
-                        length++;
-                }
-                else
-                {
-                        free(*h);
-                        *h = NULL;
-                        length++;
-                        break;
-                }
-        }
+	while (*h)
+	{
+		difference = *h - (*h)->next;
+		if (difference > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			length++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			length++;
+			break;
+		}
+	}
 
-        *h = NULL;
+	*h = NULL;
 
-        return (length);
+	return (length);
 }
