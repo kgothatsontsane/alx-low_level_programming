9e6c2bfd08cec64583bd0c898b996ded87fdccf6
Kgothatso <nvisionfactory@gmail.com>
2023-10-24 05:06:20 -0700

    added task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: the linked listint_t list whose number of elements is to be counted.
+ *
+ * Return: the number of elements in listint_t h
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h->next;
+	}
+
+	return (count);
+}
