8fae491fc505d13d79ea588b2009c830f29c48a1
Kgothatso <nvisionfactory@gmail.com>
2023-10-24 13:01:25 -0700

    edited task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (count = 0; temp && count < idx; i++)
+	for (count = 0; temp && count < idx; count++)
 	{
 		if (count == idx - 1)
 		{
