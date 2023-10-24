d60e8302a051216c998d6e2b59cba74197740781
Kgothatso <nvisionfactory@gmail.com>
2023-10-24 05:09:29 -0700

    edited task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -13,7 +11,7 @@ size_t listint_len(const listint_t *h)
 {
 	int count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		count++;
 		h->next;
