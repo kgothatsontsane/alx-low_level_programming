d9f9fd5678f036cb77b298705839f82493fb1b2f
Kgothatso <nvisionfactory@gmail.com>
2023-10-24 20:21:28 -0700

    added task 4

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+`/* 3-main.c */
+
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point for a simple calculator program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line arguments.
+ *
+ * Return: 0 on success, or exit with error codes 98 or 99 on failure.
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	operator = get_op_func(argv[2]);
+
+	if (!operator)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", operator(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
