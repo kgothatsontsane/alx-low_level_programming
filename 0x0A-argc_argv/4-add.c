3a76fe2cef9e8c6a4d19754e9bcedb80e91c7c53
Kgothatso <nvisionfactory@gmail.com>
2023-10-24 18:49:12 -0700

    edited task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,3 +20,23 @@ int main(int argc, char **argv)
 		printf("0\n");
 		return (0);
 	}
+
+
+	for (i = 1; argv[i]; i++)
+	{
+		n = strtol(argv[i], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+
+
+	return (0);
+}
