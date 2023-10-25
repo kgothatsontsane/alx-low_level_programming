b2c3800325209f494c94a7a876c3c426029c497e
Kgothatso <nvisionfactory@gmail.com>
2023-10-24 19:03:19 -0700

    added task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: The string's whose length is to be returned.
+ *
+ * Return: the length of the string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+
+	return (longit);
+}
