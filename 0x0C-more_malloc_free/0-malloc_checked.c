c8192061ee573d6e486d7f6cb5c44ba5c397923b
Kgothatso <nvisionfactory@gmail.com>
2023-10-24 20:34:39 -0700

    added task 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
-* malloc_checked - allocates memory using malloc
-* @b: allocated memory
-*
-* Return: a pointer to the allocated memory
-* if malloc fails exit with status 98
-*/
+ * malloc_checked - allocates memory using malloc
+ * @b: allocated memory
+ *
+ * Return: a pointer to the allocated memory
+ * if malloc fails exit with status 98
+ */
 
 void *malloc_checked(unsigned int b)
 {
-   void *i;
+	void *i;
 
 
-   i = malloc(b);
-   if (i == NULL)
-       exit(98);
-   return (i);
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	return (i);
 }
