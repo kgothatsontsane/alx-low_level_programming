23c1f85e590c0fbe11f26a0df3a6b0174398acf0
Kgothatso <nvisionfactory@gmail.com>
2023-10-24 18:01:29 -0700

    added task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: arguments vector
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
