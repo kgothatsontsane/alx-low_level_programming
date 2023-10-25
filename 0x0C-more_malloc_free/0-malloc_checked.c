daac630eab5ae83400974b8ae51cab724c6e792c
Kgothatso <nvisionfactory@gmail.com>
2023-10-24 20:31:10 -0700

    added task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* malloc_checked - allocates memory using malloc
+* @b: allocated memory
+*
+* Return: a pointer to the allocated memory
+* if malloc fails exit with status 98
+*/
+
+void *malloc_checked(unsigned int b)
+{
+   void *i;
+
+
+   i = malloc(b);
+   if (i == NULL)
+       exit(98);
+   return (i);
+}
